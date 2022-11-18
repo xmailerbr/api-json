4559f3cef17e422071987ee98f11ea8ef6af3082
xMailer Dev <118564049+xmailerbr@users.noreply.github.com>
2022-11-18 17:04:26 -0300

    Create envio.java

---
+++ java/envio.java
@@ -0,0 +1,16 @@
+URL url = new URL("https://api.xmailer.com.br/");
+HttpURLConnection http = (HttpURLConnection)url.openConnection();
+http.setRequestMethod("POST");
+http.setDoOutput(true);
+http.setRequestProperty("Authorization", "Content-Type: application/json");
+http.setRequestProperty("Content-Type", "application/json");
+
+String data = "{\"host_smtp\": \"HOST-SMTP\",\"usuario_smtp\": \"USUARIO-SMTP\", \"senha_smtp\": \"SENHA-SMTP\", \"emailRemetente\": \"EMAIL-REMETENTE\", \"nomeRemetente\": \"NOME-REMETENTE\",\"emailDestino\": [\"postmaster@xmailer.com.br\",\"dev@xmailer.com.br\"], \"assunto\": \"Teste de envio via API JSON\",\n\"mensagem\": \"mensagem de teste da API JSON\"}";
+
+byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+OutputStream stream = http.getOutputStream();
+stream.write(out);
+
+System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+http.disconnect();
