99c8fc1c1be02d2c3b7e0c13e7c25d0f327c93bf
xMailer Dev <118564049+xmailerbr@users.noreply.github.com>
2022-11-18 17:28:14 -0300

    Create envio.py

---
+++ python/envio.py
@@ -0,0 +1,18 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+
+url = "https://api.xmailer.com.br/"
+
+headers = CaseInsensitiveDict()
+headers["Authorization"] = "Content-Type: application/json"
+headers["Content-Type"] = "application/json"
+
+data = """
+{"host_smtp": "HOST-SMTP","usuario_smtp": "USUARIO-SMTP", "senha_smtp": "SENHA-SMTP", "emailRemetente": "EMAIL-REMETENTE", "nomeRemetente": "NOME-REMETENTE","emailDestino": ["postmaster@xmailer.com.br","dev@xmailer.com.br"], "assunto": "Teste de envio via API JSON",
+"mensagem": "mensagem de teste da API JSON"}
+"""
+
+
+resp = requests.post(url, headers=headers, data=data)
+
+print(resp.status_code)
